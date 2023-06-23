7666c97fcab7aa311d3dabe8df53ea97ee78bc16
isaacbrds <isaac.brigido@gmail.com>
2023-06-23 20:15:45 -0300

    Inicio do desafio de logica com python

---
+++ exercicio-1.py
@@ -0,0 +1,7 @@
+nome = input('Digite seu nome: ')
+telefone = input('Digite seu Telefone: ')
+endereco = input('Digite seu Endereco: ')
+
+print("Nome: "+ nome )
+print("Telefone: "+ telefone )
+print("Endereço: "+ endereco )
