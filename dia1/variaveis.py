b4a93ba42e4d1713d14018c17f05a4232b1b5e36
isaacbrds <isaac.brigido@gmail.com>
2023-06-24 11:31:51 -0300

    Ajustando pastas do desafio de logica com python

---
+++ dia1/variaveis.py
@@ -0,0 +1,5 @@
+print('Hello World!')
+var = 1
+x = input(f'teste de {var}\n')
+
+print(x)
---
+++ dia3/exercicio.py
@@ -0,0 +1,18 @@
+print("============= SISTEMA DE COMBUSTÍVEIS ======================")
+quantidade_do_tanque_do_posto = input('Digite a quantidade total de litros do tanque: ')
+quantidade_a_abastecer_no_posto = input('Digite a quantidade de litros a abastecer: ')
+
+quantidade_apos_abastecimento = int(quantidade_do_tanque_do_posto) + int(quantidade_a_abastecer_no_posto)
+print(f'A quantidade de litros que ficou no posto foi: {quantidade_apos_abastecimento}')
+
+print("Chegou cliente bora vender?")
+
+nome = input('Olá senhor qual o seu nome? ')
+valor_combustivel = int(input(f'Qual o valor que o senhor(a) {nome} deseja colocar? '))
+
+resultado = quantidade_apos_abastecimento - valor_combustivel
+
+porcentagem = float(quantidade_apos_abastecimento - valor_combustivel)/100
+
+print(f'O posto ficou com {resultado} no tanque do posto')
+print(f'O posto ficou com {porcentagem*100:.0f}% de sua capacidade ')
